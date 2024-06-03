a9f87e38cda043ed136bd0e295f507cccf154610
clement muchai <clementmuchaimoko@gmail.com>
2024-06-03 11:32:43 +0300

    ptyhon

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,2 +1,14 @@
-def lookup(object):
-    return dir(object)
+#!/usr/bin/python3
+""" Module that has a function to return a list of availble attributes and
+methods
+"""
+
+
+def lookup(obj):
+    """Function that returns the list of available attributes and methods of an
+    object
+    Args:
+        obj - the object to check
+    """
+
+    return dir(obj)
