c205ddd2700c2c80101603953b5f58114dffbab0
clement muchai <clementmuchaimoko@gmail.com>
2024-06-19 00:13:39 +0300

    code

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""basegeometry"""
+
+class BaseGeometry:
+    """instance of geometry"""
+    def __init__(seLf):
+        pass
+
+    def area(self):
+        """raise exception"""
+        raise Exception("area() is not implmented")
