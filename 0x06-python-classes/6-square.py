b6b62f02e1b0c1bb1ba42051ed43375f13235bc0
clement muchai <clementmuchaimoko@gmail.com>
2024-06-21 11:23:45 +0300

    code

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,79 +1,72 @@
 #!/usr/bin/python3
 """
-6-square
-- contains class Square that defines a square
-- returns the current square area
-- prints in stdout the square with the character #
+square class
 """
 
 
 class Square:
-    """
-    - creates __init__ method and area method
-    - creates getter and setter for size
-    - prints in stdout the square with the character #
-    """
+
     def __init__(self, size=0, position=(0, 0)):
-        """ initializes instance attribute size and position """
         self.size = size
         self.position = position
-
+    """
+    creates a square object
+    """
     @property
     def size(self):
-        """ retrieves size """
         return self.__size
+        """
+        gets size
+        """
+    @property
+    def position(self):
+        return self.__position
+        """
+        gets position
+        """
+    @position.setter
+    def position(self, value):
+        if(type(value) is not tuple or len(value) is not 2 or
+           type(value[0]) is not int or
+           type(value[1]) is not int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if(value[0] < 0 or value[1] < 0):
+            raise ValueError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
+        """
+        sets position
+        position has to be a tuple of positive integers
+        Raise:
+            ValueError
+            TypeError
+        """
     @size.setter
     def size(self, value):
-        """
-        - sets size
-        - raise errors when value is not int or value is less than 0
-        """
-        if isinstance(value, int) is not True:
+        if(type(value) is not int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif(value < 0):
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-
-    @property
-    def position(self):
-        """ retrieves position """
-        return self.__position
-
-    @position.setter
-    def position(self, value):
         """
-        - sets position
-        - raise errors when value is not tuple of 2 positive integers
+        sets size
+        size has to be an integer and positive
         """
-        if isinstance(value, tuple) is not True:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if isinstance(value[0], int) is not True:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if isinstance(value[1], int) is not True:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
 
     def area(self):
-        """ returns the current square area """
-        return self.__size * self.__size
-
+        return(self.__size**2)
+        """
+        returns the area of the size of the square
+        """
     def my_print(self):
-        """ prints in stdout the square with the character # """
-        if self.__size == 0:
-            print("")
-        else:
-            for p in range(self.__position[1]):
-                print("")
-            for i in range(self.__size):
-                for j in range(self.__position[0]):
-                    print("", end=" ")
-                for k in range(self.__size):
-                        print("#", end="")
-                print("")
+        if(self.size == 0):
+            print()
+            return
+        for x in range(self.position[1]):
+            print()
+        for x in range(self.size):
+            print("{}{}".format(" " * self.position[0], "#" * self.size))
+        """
+        prints a square of hashtags based on position and size
+        """
