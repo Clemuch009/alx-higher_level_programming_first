039630dbdcdda84b48cc51a81337d346a4b34b20
clement muchai <clementmuchaimoko@gmail.com>
2024-06-28 01:56:51 +0300

    class

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -10,13 +10,13 @@ class Rectangle:
         self.area = 0
         self.perimeter = 0
 
-   
+
     @property
     def width(self):
         """get heigth"""
         return self._width
 
-    
+
     @width.setter
     def width(self, value):
         """verifying if is integer"""
@@ -29,34 +29,10 @@ class Rectangle:
             raise ValueError ("width must be >= 0")
         self._width = value
 
-    
+
 
     @property
     def height(self):
         """get heigth"""
         return self._height
 
-    
-    @height.setter
-    def height(self, value):
-        """set height"""
-        if not isinstance(value , int):
-            """raise warning"""
-            raise TypeError ("height must be an integer")
-        if (height < 0):
-            """raise warning"""
-            raise ValueError ("height must be >= 0")
-        self._heigth = value
-
-    def area(self):
-        """ find area"""
-        area = width * width
-        return area
-
-    def perimeter (self):
-        """get perimeter"""
-        perimeter = (width + heigth) * 2
-        if width == 0 or height == 0:
-            """icf"""
-            perimeter = 0
-        return perimeter
