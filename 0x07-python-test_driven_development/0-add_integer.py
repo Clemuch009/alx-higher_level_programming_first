7418eba874ef1a6cc32f72cd2c7a55fa37153888
clement muchai <clementmuchaimoko@gmail.com>
2024-07-02 23:07:28 +0300

    code

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
+"""adds integer
 """
-adding
 
-"""
-def add_integer(a , b=98):
+
+def add_integer(a, b=98):
+    """Returns a + b
     """
-    addation
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
 
-    """
-    if not isinstance(a ,(float, int)):
-        """
-        warning
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
 
-        """
-        raise TypeError ("a must be an integer")
-
-    if not isinstance(b ,(float ,int)):
-        """
-        warning
-
-        """
-        raise TypeError ("b must be an integer")
-
-    return int(a) + int (b)
+    return int(a) + int(b)
